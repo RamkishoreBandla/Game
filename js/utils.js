37f21dcae916f8edfbaba1cec7bdd1358c4ad687
RamkishoreBandla <bandlaramkishore090@gmail.com>
2024-04-13 16:56:38 -0400

    attacks

--- js/utils.js
+++ js/utils.js
@@ -16,7 +16,7 @@ function determineWinner({ player, enemy, timerId }) {
         document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
     }
     else if (player.health < enemy.health) {
-        document.querySelector("#displaText").innerHTML = 'Player 2 Wins';
+        document.querySelector("#displayText").innerHTML = 'Player 2 Wins';
     }
 }
 
